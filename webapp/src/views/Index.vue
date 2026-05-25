ad60d51b7b5a7af886b836db7e68145c4543e23a
zhaobowen <zhongkao0621@outlook.com>
2026-05-26 01:31:44 +0800

    add connection status display and error handling

--- webapp/src/views/Index.vue
+++ webapp/src/views/Index.vue
@@ -1,15 +1,21 @@
 <template>
   <div style="height: 100%; width: 100%;">
 
-    <el-tabs v-model="activeDocumentType">
-      <el-tab-pane label="零件" name="PartDocument" :disabled="activeDocumentType !== 'PartDocument'"></el-tab-pane>
-      <el-tab-pane label="装配" name="AssemblyDocument" :disabled="activeDocumentType !== 'AssemblyDocument'"></el-tab-pane>
-      <!--<el-tab-pane label="工程图" name="DrawingDocument" :disabled="activeDocumentType !== 'DrawingDocument'"></el-tab-pane>-->
-    </el-tabs>
+    <div v-if="!connected" style="padding: 20px; color: #666;">
+      <p>{{ statusText }}</p>
+      <p v-if="errorMsg" style="color: red;">{{ errorMsg }}</p>
+    </div>
 
-    <template>
-      <component :is="currentPane"></component>
-    </template>
+    <div v-if="connected">
+      <el-tabs v-model="activeDocumentType">
+        <el-tab-pane label="零件" name="PartDocument" :disabled="activeDocumentType !== 'PartDocument'"></el-tab-pane>
+        <el-tab-pane label="装配" name="AssemblyDocument" :disabled="activeDocumentType !== 'AssemblyDocument'"></el-tab-pane>
+      </el-tabs>
+
+      <template>
+        <component :is="currentPane"></component>
+      </template>
+    </div>
 
   </div>
 </template>
@@ -32,17 +38,32 @@ export default {
     return {
       activeDocumentType: '',
       connected: false,
+      statusText: '正在连接 CrownCAD...',
+      errorMsg: '',
     }
   },
   mounted() {
-    ccPlugin.connect((result) => {
-      let docName = result.docInfo.name;
-      let docType = result.docInfo.type;
-      messageTip.successTip("插件初始化完成。当前文档名称: " + docName);
-      commandManager.docType = docType;
-      this.activeDocumentType = docType;
-      this.connected = true;
-    });
+    try {
+      ccPlugin.connect((result) => {
+        try {
+          let docName = result.docInfo.name;
+          let docType = result.docInfo.type;
+          messageTip.successTip("插件初始化完成。当前文档名称: " + docName);
+          commandManager.docType = docType;
+          this.activeDocumentType = docType;
+          this.connected = true;
+        } catch (e) {
+          this.errorMsg = '数据解析错误: ' + e.message;
+          this.statusText = '连接失败';
+        }
+      }).catch(err => {
+        this.errorMsg = '连接失败: ' + (err?.message || err);
+        this.statusText = '连接失败';
+      });
+    } catch (e) {
+      this.errorMsg = 'SDK 加载失败: ' + e.message;
+      this.statusText = '初始化失败';
+    }
   },
   methods: {
 
